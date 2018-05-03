472e795889c1061992cdcbdb0c71da2f840a57f1
Robert Goon <rob.goon@gmail.com>
2018-05-03 17:45:58 -0400

    resized player model, used P2JS methods, added const movementSpeed, added comments

--- client/level1State.js
+++ client/level1State.js
@@ -2,19 +2,28 @@
 
 let player;
 let keybinds = {};
+const movementSpeed = 150;
 
 export default {
   create() {
+    // P2JS not started by default
     D6Dungeon.game.physics.startSystem(Phaser.Physics.P2JS);
 
+    // *** Player - Sprite ***
     player = D6Dungeon.game.add.sprite(500, 450, 'player');
     player.anchor.setTo(0.5, 0.5);
-    D6Dungeon.game.physics.enable(player, Phaser.Physics.P2JS);
-
     player.scale.set(4);
 
+    // *** Player - Physics ***
+    D6Dungeon.game.physics.enable(player, Phaser.Physics.P2JS);
+    player.body.fixedRotation = true;
+    player.body.setRectangle(player.width - 10, player.height - 10, 0, 6);
+    // player.body.debug = true // Use to see collision model
+
+    // *** Player - Animation ***
     player.animations.add('walk', null, 10, true);
 
+    // *** Player - Keybinds ***
     keybinds.up = D6Dungeon.game.input.keyboard.addKey(Phaser.Keyboard.W);
     keybinds.down = D6Dungeon.game.input.keyboard.addKey(Phaser.Keyboard.S);
     keybinds.left = D6Dungeon.game.input.keyboard.addKey(Phaser.Keyboard.A);
@@ -22,27 +31,25 @@ export default {
     keybinds.arrows = D6Dungeon.game.input.keyboard.createCursorKeys();
   },
 
-  update(){
+  update() {
     player.body.velocity.x = 0;
     player.body.velocity.y = 0;
 
     if (keybinds.up.isDown) {
-      player.body.velocity.y = -150;
-    }
-    else if (keybinds.down.isDown) {
-      player.body.velocity.y = 150;
+      player.body.moveUp(movementSpeed);
+    } else if (keybinds.down.isDown) {
+      player.body.moveDown(movementSpeed);
     }
 
     if (keybinds.left.isDown) {
-      player.body.velocity.x = -150;
+      player.body.moveLeft(movementSpeed);
 
       // Flips player to face left
       if (player.scale.x < 0) {
         player.scale.x *= -1;
       }
-    }
-    else if (keybinds.right.isDown) {
-      player.body.velocity.x = 150;
+    } else if (keybinds.right.isDown) {
+      player.body.moveRight(movementSpeed);
 
       // Flips player to face right
       if (player.scale.x > 0) {
@@ -52,8 +59,7 @@ export default {
 
     if (player.body.velocity.x === 0 && player.body.velocity.y === 0) {
       player.animations.stop('walk', true);
-    }
-    else {
+    } else {
       player.animations.play('walk');
     }
 
@@ -63,8 +69,7 @@ export default {
       if (player.scale.x < 0) {
         player.scale.x *= -1;
       }
-    }
-    else if (keybinds.arrows.right.isDown) {
+    } else if (keybinds.arrows.right.isDown) {
       // Flips player to face right
       if (player.scale.x > 0) {
         player.scale.x *= -1;
