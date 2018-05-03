7e2636ed3a3899407ac021a51e66e5cbe9e8b44c
eugenepark2014 <eugenepark2014@gmail.com>
2018-05-03 17:03:29 -0400

    enemies spawn

--- client/level1State.js
+++ client/level1State.js
@@ -1,12 +1,29 @@
 /* global D6Dungeon */
-
+import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js';
+import { Weasel } from './components/enemies';
 let player;
 let cursors;
+let enemies;
 let facing = 'left';
+let enemyPos = {
+  pos0: [
+    { x: 300, y: 300 },
+    { x: 300, y: 600 },
+    { x: 600, y: 300 },
+    { x: 600, y: 600 }
+  ],
+  pos1: [
+    { x: 200, y: 800 },
+    { x: 200, y: 200 },
+    { x: 200, y: 400 },
+    { x: 200, y: 600 }
+  ]
+};
 
 export default {
   create() {
     player = D6Dungeon.game.add.sprite(500, 450, 'player');
+
     player.anchor.setTo(0.5, 0.5);
     D6Dungeon.game.physics.enable(player, Phaser.Physics.ARCADE);
 
@@ -15,9 +32,15 @@ export default {
     player.animations.add('walk', null, 10, true);
 
     cursors = D6Dungeon.game.input.keyboard.createCursorKeys();
+    enemies = [];
+    var ran = Math.floor(Math.random()*2);
+    console.log(ran);
+    enemyPos[`pos${ran}`].forEach(pos => {
+      enemies.push(new Weasel(D6Dungeon.game, pos.x, pos.y));
+    });
   },
 
-  update(){
+  update() {
     player.body.velocity.x = 0;
 
     if (cursors.left.isDown) {
@@ -32,8 +55,7 @@ export default {
       if (player.scale.x < 0) {
         player.scale.x *= -1;
       }
-    }
-    else if (cursors.right.isDown) {
+    } else if (cursors.right.isDown) {
       player.body.velocity.x = 150;
 
       if (facing != 'right') {
@@ -45,8 +67,7 @@ export default {
       if (player.scale.x > 0) {
         player.scale.x *= -1;
       }
-    }
-    else {
+    } else {
       if (facing != 'idle') {
         player.animations.stop();
 
