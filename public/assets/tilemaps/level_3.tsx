2eae1fddf10297f67d1bc24beeecb58b4694f9af
Tyler Brooks <tkbrooks.github@gmail.com>
2018-05-03 09:04:01 -0700

    changed file names

--- public/assets/tilemaps/level_3.tsx
+++ public/assets/tilemaps/level_3.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="level_3" tilewidth="64" tileheight="64" tilecount="256" columns="16">
- <image source="64x64 Dungeon/Dungeon@64x64.png" trans="ff00ff" width="1024" height="1024"/>
+ <image source="64x64 Dungeon/level_3.png" trans="ff00ff" width="1024" height="1024"/>
 </tileset>
