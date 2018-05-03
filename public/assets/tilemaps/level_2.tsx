ea99c5af523dd57180a403c94f26d9e677a04900
Tyler Brooks <tkbrooks.github@gmail.com>
2018-05-03 08:58:05 -0700

    changed gitignore to not ignore all tilemaps folders

---
+++ public/assets/tilemaps/level_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="level_2" tilewidth="64" tileheight="64" tilecount="256" columns="16">
+ <image source="64x64 Dungeon/DungeonColor3@64x64.png" width="1024" height="1024"/>
+</tileset>
