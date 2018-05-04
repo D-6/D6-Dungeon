68ab482344a3d0f611ee7711ab51e32ff328fdf5
Tyler Brooks <tkbrooks.github@gmail.com>
2018-05-03 20:52:41 -0700

    added all level 1 tilemaps

--- server/api/levels.js
+++ server/api/levels.js
@@ -11,7 +11,7 @@ router.get('/:level', async (req, res, next) => {
     let newMap;
     switch (level) {
       case '1':
-        newMap = new Map(7, 2, true);
+        newMap = new Map(7, 8, true);
         break;
       case '2':
         newMap = new Map(7, 12, true);
@@ -23,52 +23,27 @@ router.get('/:level', async (req, res, next) => {
         newMap = null;
     }
 
-    // let map = D6Dungeon.game.add.tilemap('level1map');
-    // map.addTilesetImage('level_1', 'level1image');
-    // const floor = map.createLayer('Floor');
-    // const walls = map.createLayer('Walls');
+    const promiseArray = newMap.rooms.map(room => {
+      const pathToFile = path.join(
+        __dirname,
+        '..',
+        'map_generator',
+        'layouts',
+        room.filename
+      );
+      return readFilePromise(pathToFile, 'utf8');
+    });
 
-    // const data = await newMap.createJSONMap();
-    // console.log(data);
-
-    // combineRooms(newMap);
-
-    // console.log(newMap.rooms);
-
-    const pathToFile = path.join(
-      __dirname,
-      '..',
-      'map_generator',
-      'layouts',
-      newMap.rooms[0].filename
-    );
-
-    let data = await readFilePromise(pathToFile, 'utf8');
-
-    data = JSON.parse(data);
-
-    res.json(data);
-
-    // const promiseArray = newMap.rooms.map(room => {
-    //   const pathToFile = path.join(
-    //     __dirname,
-    //     '..',
-    //     'map_generator',
-    //     'layouts',
-    //     room.filename
-    //   );
-    //   return readFilePromise(pathToFile, 'utf8');
-    // });
-    // Promise.all(promiseArray)
-    //   .then(rooms =>
-    //     rooms.map((room, i) => {
-    //       const JSONroom = JSON.parse(room);
-    //       JSONroom.position = newMap.rooms[i].position;
-    //       return JSONroom;
-    //     })
-    //   )
-    //   .then(rooms => res.json(rooms))
-    //   .catch(err => next(err));
+    Promise.all(promiseArray)
+      .then(rooms =>
+        rooms.map((room, i) => {
+          const JSONroom = JSON.parse(room);
+          JSONroom.position = newMap.rooms[i].position;
+          return JSONroom;
+        })
+      )
+      .then(rooms => res.json(rooms))
+      .catch(err => next(err));
   } catch (err) {
     next(err);
   }
