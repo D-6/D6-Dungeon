c8a46e15849b95aa9169402ec7c5bfa2fed9fede
Tyler Brooks <tkbrooks.github@gmail.com>
2018-05-04 19:10:17 -0700

    finished door sensors

---
+++ client/doorSensors.js
@@ -0,0 +1,31 @@
+const createSensor = (game, x, y) => {
+  const sensor = game.add.sprite(x, y, 'wizard');
+  sensor.scale.set(0.1);
+
+  // 2nd arg is debug mode
+  game.physics.p2.enable(sensor, true);
+  sensor.body.static = true;
+  sensor.body.addCircle(85).sensor = true;
+
+  return sensor;
+};
+
+export const createDoorSensors = game => {
+  const sensorWest = createSensor(game, 32, 416);
+  const sensorEast = createSensor(game, 1184, 416);
+  const sensorNorth = createSensor(game, 608, 32);
+  const sensorSouth = createSensor(game, 608, 800);
+
+  sensorWest.body.onBeginContact.add(other => {
+    console.log('West door entered by:', other.sprite.key);
+  });
+  sensorEast.body.onBeginContact.add(other => {
+    console.log('East door entered by:', other.sprite.key);
+  });
+  sensorNorth.body.onBeginContact.add(other => {
+    console.log('North door entered by:', other.sprite.key);
+  });
+  sensorSouth.body.onBeginContact.add(other => {
+    console.log('South door entered by:', other.sprite.key);
+  });
+};
