f6155ea4681735aabcd0b1786f8588232e8aa3e0
Tyler Brooks <tkbrooks.github@gmail.com>
2018-05-13 19:31:26 -0700

    changed char animations

--- client/socket.js
+++ client/socket.js
@@ -63,6 +63,11 @@ const player2Hit = ({ health }) => {
 
 const player2Move = ({ x, y }) => {
   const { player2 } = D6Dungeon.game.state;
+  if (player2.sprite.body.x < x) {
+    player2.sprite.scale.x = 1;
+  } else {
+    player2.sprite.scale.x = -1;
+  }
   player2.sprite.body.x = x;
   player2.sprite.body.y = y;
 };
