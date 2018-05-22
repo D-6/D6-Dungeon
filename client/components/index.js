736ee94b7147f2d7a46ef3ed652cbd0f92f12826
Robert Goon <rob.goon@gmail.com>
2018-05-22 15:44:04 -0400

    removed extra files and references

--- client/components/index.js
+++ client/components/index.js
@@ -3,4 +3,5 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {Login, Signup} from './auth-form'
+
+//  export { Login, Signup } from './auth-form';
